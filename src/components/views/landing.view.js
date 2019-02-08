346bb13a169417bb3c57d6bad088ed86e53527d7
Manuel Castro <maacaro@gmail.com>
2019-02-08 08:59:36 -0400

    SJF-001 render either loginForm or Dashboard component depending on the login

--- src/components/views/landing.view.js
+++ src/components/views/landing.view.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import LoginForm from "../loginForm/loginForm";
 import DashBoard from "../dashboard/dashboard";
 
-const landingView = ({ isLogin }) =>
-  isLogin ? <DashBoard /> : <LoginForm onSuccessLogin={() => {}} />;
-
-export const handleLogin = (isLoginBy, response) => {
-  if (isLoginBy === "google") {
-    return {
-      email: response.profileObj.email,
-      name: `${response.profileObj.givenName} ${
-        response.profileObj.familyName
-      }`,
-      picture: response.profileObj.imageUrl
-    };
+export default class LandingView extends Component {
+  state = {
+    isLogin: false,
+    credentials: null
+  };
+  render() {
+    const { isLogin, credentials } = this.state;
+    return isLogin ? (
+      <DashBoard credentials={{ ...credentials }} />
+    ) : (
+      <LoginForm
+        onSuccessLogin={credentials =>
+          this.setState({ isLogin: true, credentials: { ...credentials } })
+        }
+      />
+    );
   }
-  if (isLoginBy === "facebook") {
-    return {
-      name: response.name,
-      email: response.email,
-      picture: response.picture.data.url
-    };
-  }
-};
-export default landingView;
+}
--- src/components/views/landing.view.test.js
+++ src/components/views/landing.view.test.js
@@ -3,64 +3,31 @@ import { shallow } from "enzyme";
 import LandingView from "./landing.view";
 import LoginForm from "../loginForm/loginForm";
 import DashBoard from "../dashboard/dashboard";
-import { handleLogin } from "./landing.view";
 
 describe("Landing View", () => {
   it("should be defined", () => {
     const wrapper = shallow(<LandingView />);
     expect(wrapper).toBeDefined();
   });
-  it("should contain a LoginForm Component when is not login", () => {
-    const wrapper = shallow(<LandingView isLogin={false} />);
+  it("should render a LoginForm Component", () => {
+    const wrapper = shallow(<LandingView />);
     expect(wrapper.find(LoginForm)).toHaveLength(1);
   });
-  it("should have a onSuccessLogin prop set to a function", () => {
+  it("should render a dashboard on Successful Login ", () => {
     const wrapper = shallow(<LandingView />);
-    expect(wrapper.find(LoginForm).props().onSuccessLogin).toEqual(
-      expect.any(Function)
-    );
-  });
-  it("should not contain a LoginFrom when the user is Login", () => {
-    const wrapper = shallow(<LandingView isLogin={true} />);
-    expect(wrapper.find(LoginForm)).toHaveLength(0);
-  });
-  it("should not contain a DashBoard when the user is Login", () => {
-    const wrapper = shallow(<LandingView isLogin={true} />);
+    wrapper
+      .find(LoginForm)
+      .props()
+      .onSuccessLogin();
     expect(wrapper.find(DashBoard)).toHaveLength(1);
   });
-  describe("the handleLogin function", () => {
-    describe("when the login by google is succesful", () => {
-      it("should return a object {name, email,picture}", () => {
-        const googleResponse = {
-          profileObj: {
-            email: "maacaro@gmail.com",
-            imageUrl: "someURL",
-            givenName: "Manuel",
-            familyName: "Castro"
-          }
-        };
-        const userInfo = handleLogin("google", googleResponse);
-        expect(userInfo).toEqual({
-          name: "Manuel Castro",
-          email: "maacaro@gmail.com",
-          picture: "someURL"
-        });
-      });
-    });
-    describe("when the login by facebook is succesful", () => {
-      it("should return a object {name, email,picture}", () => {
-        const responseFacebook = {
-          name: "Manuel Castro",
-          email: "maacaro@gmail.com",
-          picture: { data: { url: "someURL" } }
-        };
-        const userInfo = handleLogin("facebook", responseFacebook);
-        expect(userInfo).toEqual({
-          name: "Manuel Castro",
-          email: "maacaro@gmail.com",
-          picture: "someURL"
-        });
-      });
-    });
+  it("should render a dashboard with credentials props on Successful Login ", () => {
+    const wrapper = shallow(<LandingView />);
+    const credentials = { foo: "foo", bar: "bar" };
+    wrapper
+      .find(LoginForm)
+      .props()
+      .onSuccessLogin(credentials);
+    expect(wrapper.find(DashBoard).props().credentials).toEqual(credentials);
   });
 });
